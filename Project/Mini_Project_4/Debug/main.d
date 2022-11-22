b91405a761a1644b3e634bf0ea6ca6a42c85c168
Mohamed Elshaer <mohamedelshaer82@gmail.com>
2022-11-22 17:00:07 +0200

    Temp_Sensor

---
+++ Project/Mini_Project_4/Debug/main.d
@@ -0,0 +1,14 @@
+main.o main.o: ../main.c ../Ultrasonic.h ../std_types.h ../gpio.h \
+  ../lcd.h ../common_macros.h ../icu.h
+
+../Ultrasonic.h:
+
+../std_types.h:
+
+../gpio.h:
+
+../lcd.h:
+
+../common_macros.h:
+
+../icu.h:
